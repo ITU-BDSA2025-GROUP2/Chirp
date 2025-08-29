b02c4123e734ef98cc591f5a8a05df0c1071cf75
Tim <thou@itu.dk>
2025-08-29 08:35:02 +0200

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+if (args[0] == "say")
+{
+    var message = args[1];
+    var freaquency = int.Parse(args[2]);
+    foreach (var i in Enumerable.Range(1, freaquency))
+        Console.WriteLine(message + " ");
+}
