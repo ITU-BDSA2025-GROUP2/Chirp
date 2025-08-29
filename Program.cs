6ddc85e841d6d9344b3ca8ec98c38537fa2c366e
Tim <thou@itu.dk>
2025-08-29 08:47:17 +0200

    new commit Co-authoured by me

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@ if (args[0] == "say")
 {
     var message = args[1];
     var freaquency = int.Parse(args[2]);
-    foreach (var i in Enumerable.Range(1, freaquency))
+    foreach (var arg in args)
         Console.WriteLine(message + " ");
 }
