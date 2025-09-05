b496c9a70751ffa3b43cdc6356374253c364ae09
msso-Mads <msso@itu.dk>
2025-09-05 10:21:33 +0200

    Exercise 2.c complete.
    User Interface code now resides in the UserInterface class

--- Program.cs
+++ Program.cs
@@ -14,15 +14,19 @@ using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
     }
 }
 
+
+
 if (args[0] == "read")
 {
-    foreach (var rs in records)
+    UserInterface.PrintCheeps(records);
+
+    /* foreach (var rs in records)
     {
         DateTimeOffset dataTimeOffSet = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(rs.Timestamp));
         DateTime time = dataTimeOffSet.DateTime;
         Console.WriteLine(rs.Author + " @ " + time + " " + rs.Message);
-    }
-}  
+    } */
+}
 else if (args[0] == "cheep")
 {
     long time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
@@ -37,11 +41,11 @@ else if (args[0] == "cheep")
         msg = msg + " " + arg;
     }
 
-    records.Add(new Messages { Author = name, Message = msg, Timestamp = time.ToString()});
-    
+    records.Add(new Messages { Author = name, Message = msg, Timestamp = time.ToString() });
+
     using (var writer = new StreamWriter("chirp_cli_db.csv"))
     using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
     {
-        csvWriter.WriteRecords(records); 
+        csvWriter.WriteRecords(records);
     }
 }
