1336301d310871eca5197de2d1c0bd0a432208f9
Tim <thou@itu.dk>
2025-09-05 09:48:25 +0200

    Refactored read to be inside the if statement

--- Program.cs
+++ Program.cs
@@ -3,26 +3,23 @@ using Microsoft.VisualBasic;
 using System.Globalization;
 List<Messages> records = new List<Messages>();
 
-using (var reader = new StreamReader("chirp_cli_db.csv"))
-using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-{
-    csv.Context.RegisterClassMap<csvMessageMapping>();
-    var record = csv.GetRecords<Messages>();
-    foreach (var message in record)
-    {
-        records.Add(message);
-    }
-}
 
 if (args[0] == "read")
 {
-    foreach (var rs in records)
+    //public IEnumerable<T> Read(int? limit = null) {
+    using var reader = new StreamReader("chirp_cli_db.csv");
+    using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+    csv.Context.RegisterClassMap<csvMessageMapping>();
+    var record = csv.GetRecords<Messages>();
+    
+    foreach (var rs in record)
     {
         DateTimeOffset dataTimeOffSet = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(rs.Timestamp));
         DateTime time = dataTimeOffSet.DateTime;
         Console.WriteLine(rs.Author + " @ " + time + " " + rs.Message);
     }
-}  
+    //}
+}
 else if (args[0] == "cheep")
 {
     long time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
@@ -37,11 +34,11 @@ else if (args[0] == "cheep")
         msg = msg + " " + arg;
     }
 
-    records.Add(new Messages { Author = name, Message = msg, Timestamp = time.ToString()});
-    
+    records.Add(new Messages { Author = name, Message = msg, Timestamp = time.ToString() });
+
     using (var writer = new StreamWriter("chirp_cli_db.csv"))
     using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
     {
-        csvWriter.WriteRecords(records); 
+        csvWriter.WriteRecords(records);
     }
 }
