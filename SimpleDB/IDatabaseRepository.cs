1721e14e367ee0ddac830e8352ee649b664e5229
KaramTNC <karamhaybout@gmail.com>
2025-09-05 10:03:18 +0200

    SimpleDB class library
    Implement a new class library for handling .csv read/write operations
    
    We decided to begin refactoring the csv parser logic out of the main code and into a separate library for easier access and maintenance

---
+++ SimpleDB/IDatabaseRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleDB;
+
+public interface IDatabaseRepository<T>
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="limit"></param>
+    /// <returns></returns>
+    public IEnumerable<T> Read(int? limit = null);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="record"></param>
+    public void Store(T record);
+}
