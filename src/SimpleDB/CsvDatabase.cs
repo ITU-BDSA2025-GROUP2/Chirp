17f08619a1f819a03556a4fac99fa103e8c614a8
dakl <dakl@itu.dk>
2025-09-12 12:24:02 +0200

    E2E test Implement
    Implement E2E test.
    Fix filepath issues

--- src/SimpleDB/CsvDatabase.cs
+++ src/SimpleDB/CsvDatabase.cs
@@ -6,9 +6,33 @@ using System.Globalization;
 
 public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 {
+    //code for singleton implementation taken from: https://csharpindepth.com/Articles/Singleton
+    private static CSVDatabase<T> instance = null;
+    private static readonly object padlock = new object();
+
+    public static CSVDatabase<T> Instance
+    {
+        get
+        {
+            //ensures that another instance cannot be created while the process of making sure only on instance is present is still running
+            //can be removed as it can affect performance (will just be less secure if we work with threads)
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new CSVDatabase<T>();
+                }
+                return instance;
+            }
+        }
+    }
+
+
     public IEnumerable<T> Read(int? limit = null)
     {
-        using var reader = new StreamReader("../SimpleDB/chirp_cli_db.csv");
+
+        string filepath = Path.Combine(AppContext.BaseDirectory, "data", "chirp_cli_db.csv");
+        using var reader = new StreamReader(filepath);
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
         csv.Context.RegisterClassMap<csvMessageMapping>();
         var record = csv.GetRecords<Messages>().ToList();
@@ -18,8 +42,8 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 
     public void Store(T record)
     {
-
-        using var writer = new StreamWriter("../SimpleDB/chirp_cli_db.csv", true);
+        string filepath = Path.Combine(AppContext.BaseDirectory, "data", "chirp_cli_db.csv");
+        using var writer = new StreamWriter(filepath, true);
         using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
 
         csvWriter.WriteRecord(record);
--- test/Chirp.CLI.Client.Tests/E2ETest1.cs
+++ test/Chirp.CLI.Client.Tests/E2ETest1.cs
@@ -9,28 +9,15 @@ public class E2ETest1
 
 
     [Fact]
-    public void TestReadCheep()
-    {
-        // Arrange
-        // Act
-        string output = "";
-        using (var process = new Process())
-        {
-            process.StartInfo.FileName = "/usr/bin/dotnet";
-            process.StartInfo.Arguments = "./src/Chirp.CLI.Client/bin/Debug/net8.0/chirp.dll read";
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.WorkingDirectory = "../../../../../";
-            process.StartInfo.RedirectStandardOutput = true;
-            process.Start();
-            // Synchronously read the standard output of the spawned process.
-            StreamReader reader = process.StandardOutput;
-            output = reader.ReadToEnd();
-            process.WaitForExit();
-        }
-        string fstCheep = output.Split("\n")[0];
-        // Assert
-        Assert.StartsWith("ropf", fstCheep);
-        Assert.EndsWith("Hello, BDSA students!", fstCheep);
-    }
+  public void TestReadCheeps()
+  {
+      // Arrange
+      var args = new string[] { "read" };
+      // Act
+      var result = Program.Main(args);
+      // Assert
+      Assert.Equal(0, result);
+  }
 }
+
 
