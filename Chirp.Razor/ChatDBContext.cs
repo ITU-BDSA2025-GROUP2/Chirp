071e888b1670ed7a128f3bc01d28fa623664cae0
VeeBee <dakl.larsen@gmail.com>
2025-10-03 11:51:30 +0200

    Issue with commit messages. Last two co-authors were mistakes.
    Co-Authors were msso-Mads
    msso@itu.dk

---
+++ Chirp.Razor/ChatDBContext.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+
+public class ChatDBContext : DbContext
+{
+    DbSet<Cheep> Cheeps { get; set; }
+
+    DbSet<Author> Authors { get; set; }
+
+    public ChatDBContext(DbContextOptions<ChatDBContext> options) : base(options)
+    {
+
+    }
+}
