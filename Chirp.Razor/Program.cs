85c69c7a781dc80adcd6b48f9c2024b0c3514811
Maja-Box <madeleine.jakobsen2@gmail.com>
2025-10-07 11:09:19 +0200

    finished all integration tests co-author: @TimHounsgaard

--- Chirp.Razor/Program.cs
+++ Chirp.Razor/Program.cs
@@ -1,28 +1,36 @@
 using Chirp.Razor;
 
-var builder = WebApplication.CreateBuilder(args);
+public class Program {
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddSingleton<ICheepService, CheepService>();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddSingleton<ICheepService, CheepService>();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
+    
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapRazorPages();
-
-app.Run();
 
 
--- test/integration_test.cs
+++ test/integration_test.cs
@@ -4,23 +4,47 @@ using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using System.Net.Http;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
 using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 public class integration_test : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
+    private Program _program;
 
 
-
-    public integration_test()
+    public integration_test(WebApplicationFactory<Program> factory)
     {
+        _factory = factory;
+        _program = new Program();
+    }
 
+    /*[Fact]
+    public void ServerExists()
+    {
+        var emptyArg = new string[] { " " };
+
+
+        Assert.Equal(0, _program.Main(emptyArg));
+    }*/
+
+    [Fact]
+    public async Task GetFromSpecificAuthor()
+    {
+        HttpClient client = _factory.CreateClient();
+        var response = await client.GetAsync("/Helge");
+        response.EnsureSuccessStatusCode();
+        var readResponse = await response.Content.ReadAsStringAsync();
+        Assert.Contains("11", readResponse);
     }
 
     [Fact]
-    public void ServerExists()
+    public async Task GetAll()
     {
-        var emptyArg = new string[] {" "};
-
-        Assert.Equal(0, _program.Main(emptyArg));
+        HttpClient client = _factory.CreateClient();
+        var response = await client.GetAsync("");
+        response.EnsureSuccessStatusCode();
+        var readResponse = await response.Content.ReadAsStringAsync();
+        Assert.Contains("Leave your horses below and nerving itself to concealment.", readResponse);
     }
 }
