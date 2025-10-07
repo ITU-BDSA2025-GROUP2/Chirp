1a4f48d064c3cc9c49b4af3b09bd72635d211a9f
Tim <thou@itu.dk>
2025-10-07 11:32:05 +0200

    added unit-test to DBfacade @Maja-Box

--- test/FacadeTest.cs
+++ test/FacadeTest.cs
@@ -4,5 +4,33 @@ namespace test;
 
 public class FacadeTest
 {
-    //private readonly 
+    private readonly DBFacade _facade = new DBFacade();
+
+
+    public FacadeTest()
+    {
+        _facade.createDatabase();
+    }
+
+    [Fact]
+    public void CheckDB()
+    {
+        Assert.True(File.Exists(Path.GetTempPath() + "Chirp.db"));
+
+    }
+
+    [Fact]
+    public void CheckGetCheeps()
+    {
+        Assert.Equal(32, _facade.getAllCheeps(0).Count());
+    }
+    
+    [Fact]
+    public void CheckGetAuthorCheep()
+    {
+        Assert.Equal(1, _facade.getAllFromAuthor("Helge",0).Count());
+    }
+
+
+
 }
