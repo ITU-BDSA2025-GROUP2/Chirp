87c0913586c543e82792c5d6e3f721cdf313d22c
KaramTNC <karamhaybout@gmail.com>
2025-10-07 14:28:25 +0200

    Project restructure
    Move existing classes into src folder and organized folders

---
+++ Chirp.Razor/src/CheepRepository/CheepRepository.cs
@@ -0,0 +1,20 @@
+
+
+public class CheepRepository : ICheepRepository
+{
+    public Task CreateCheep(CheepDTO newCheep)
+    {
+        return null;
+    }
+
+
+    public Task<List<CheepDTO>> ReadCheeps(string name)
+    {
+        return null;
+    }
+
+    public Task UpdateCheep(CheepDTO alteredCheep)
+    {
+        return null;
+    }
+}
---
+++ Chirp.Razor/src/CheepService.cs
@@ -0,0 +1,41 @@
+
+public record CheepViewModel(string Author, string Message, string Timestamp);
+
+public interface ICheepService
+{
+    public List<CheepViewModel> GetCheeps(int page);
+    public List<CheepViewModel> GetCheepsFromAuthor(string author, int page);
+}
+
+public class CheepService : ICheepService
+{
+    DBFacade facade = new DBFacade();
+    
+
+    // These would normally be loaded from a database for example
+    private static readonly List<CheepViewModel> _cheeps = new()
+        {
+            new CheepViewModel("Helge", "Hello, BDSA students!", UnixTimeStampToDateTimeString(1690892208)),
+            new CheepViewModel("Adrian", "Hej, velkommen til kurset.", UnixTimeStampToDateTimeString(1690895308)),
+        };
+
+    public List<CheepViewModel> GetCheeps(int page)
+    {
+        return facade.getAllCheeps(page);
+    }
+
+    public List<CheepViewModel> GetCheepsFromAuthor(string author, int page)
+    {
+        // filter by the provided author name
+        return facade.getAllFromAuthor(author, page);
+    }
+
+    private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
+    {
+        // Unix timestamp is seconds past epoch
+        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(unixTimeStamp);
+        return dateTime.ToString("MM/dd/yy H:mm:ss");
+    }
+
+}
