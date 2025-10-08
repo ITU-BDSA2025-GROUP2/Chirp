e4c79141d89caccbc1c290365426ddd874579839
KaramTNC <karamhaybout@gmail.com>
2025-10-08 16:14:47 +0200

    Removed duplicate classes

--- Chirp.Razor/obj/Debug/net9.0/Chirp.Razor.AssemblyInfo.cs
+++ Chirp.Razor/obj/Debug/net9.0/Chirp.Razor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chirp.Razor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277c2f500d05a6bb7bd42a75e1a5ec676fdef5dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c9ffe9f2c25895c3918110ba7d0a210b95aa207")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chirp.Razor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chirp.Razor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
