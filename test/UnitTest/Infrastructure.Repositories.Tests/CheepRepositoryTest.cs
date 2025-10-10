16ffed7573e234d5d2dbf6760a1ba14c55c012cd
msso-Mads <msso@itu.dk>
2025-10-10 15:09:56 +0200

    Created in memory database @TimHounsgaard

--- test/UnitTest/Infrastructure.Repositories.Tests/CheepRepositoryTest.cs
+++ test/UnitTest/Infrastructure.Repositories.Tests/CheepRepositoryTest.cs
@@ -1,26 +1,35 @@
 
 
 using System.Reflection;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 public class CheepRepositoryTest
 {
     private CheepRepository cheepRepository;
 
+    ICheepRepository repository;
+
     public CheepRepositoryTest()
     {
-        ChatDbContext dbContext = new ChatDbContext();
-        cheepRepository = new CheepRepository(dbContext);
+        using var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<ChatDBContext>().UseSqlite(connection);
+
+        var context = new ChatDBContext(builder.Options);
+        context.Database.EnsureCreated();
+        repository = new CheepRepository(context);
     }
 
     [Fact]
     public void Test1()
     {
-        Assert.NotNull(cheepRepository);
+        Assert.NotNull(repository);
     }
-
+/* 
     [Fact]
     public async Task FindNewIdTest()
     {
         
-    }
+    } */
 }
