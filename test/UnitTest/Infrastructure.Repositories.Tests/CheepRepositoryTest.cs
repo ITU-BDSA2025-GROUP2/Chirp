7ab7bc655355c032a463c9bc0d231c8ac5350a82
msso-Mads <msso@itu.dk>
2025-10-10 15:57:18 +0200

    Added another test and added comments @TimHounsgaard

--- test/UnitTest/Infrastructure.Repositories.Tests/CheepRepositoryTest.cs
+++ test/UnitTest/Infrastructure.Repositories.Tests/CheepRepositoryTest.cs
@@ -10,14 +10,20 @@ public class CheepRepositoryTest
 
     ICheepRepository repository;
 
+    public SqliteConnection connection;
+
     public CheepRepositoryTest()
     {
-        using var connection = new SqliteConnection("Filename=:memory:");
+        // Here we generate a sqlite in memory db could be smart to make a support class.
+        // that all tests call to create a test db to reduce code duplicationæ.
+
+        connection = new SqliteConnection("Filename=:memory:");
         connection.Open();
         var builder = new DbContextOptionsBuilder<ChatDBContext>().UseSqlite(connection);
 
         var context = new ChatDBContext(builder.Options);
         context.Database.EnsureCreated();
+
         repository = new CheepRepository(context);
     }
 
@@ -26,10 +32,29 @@ public class CheepRepositoryTest
     {
         Assert.NotNull(repository);
     }
-/* 
+
     [Fact]
     public async Task FindNewIdTest()
     {
-        
-    } */
+        Assert.Equal(1, repository.FindNewId());
+    }
+
+
+    /*
+    The test below was just to verify we could add to the new data base; 
+    The test case should be updated with the queries to do the following instead of what is is currently
+    doing.
+
+    Assert.False(query author)
+    add author
+    Assert.True(query author)
+    */ 
+
+    [Fact]
+    public async Task DoesItCreateAuthor()
+    {
+        Assert.Equal(1, repository.FindNewId());
+        repository.CreateAuthor("Tim", "tim@email.com");
+        Assert.Equal(2, repository.FindNewId());
+    }
 }
