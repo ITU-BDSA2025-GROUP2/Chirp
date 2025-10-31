e672278b27a9ed8715ce30a0864a0e40908e4373
Tim (thou) <94905587+TimHounsgaard@users.noreply.github.com>
2025-10-31 21:26:28 +0100

    Update Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -42,7 +42,7 @@ public class Program
         using (var scope = app.Services.CreateScope())
         {
             var context = scope.ServiceProvider.GetRequiredService<ChatDbContext>();
-
+            context.Database.EnsureCreated();
             DbInitializer.SeedDatabase(context);
         }
 
