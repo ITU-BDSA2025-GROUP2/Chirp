1361fdadb0e00873de8226d4956df91d912ba786
Tim <thou@itu.dk>
2025-11-21 13:59:54 +0100

    Displays cheeps on usertimeline as needed

--- src/Web/Pages/UserTimeline.cshtml.cs
+++ src/Web/Pages/UserTimeline.cshtml.cs
@@ -12,12 +12,36 @@ public class UserTimelineModel(ICheepService service) : PageModel
 
     public async Task<ActionResult> OnGet(string author, [FromQuery] int page)
     {
+
+        //This would be nice to order all by timestamp instead of person. 
         var ids = await _service.GetFollowers(User.Identity.Name);
         var returnList = await _service.GetCheepsFromFollowed(ids, page);
+        var IsFollowed = false;
         Cheeps = new List<CheepViewModel>();
-        foreach (var row in returnList)
-        {
-            Cheeps.Add(new CheepViewModel(row.Author.Name, row.Text, row.TimeStamp.ToString(), row.Author.Email, "Follow"));
+        if (author == User.Identity.Name)
+        {   
+            foreach (var row in returnList)
+            {
+                var id = row.Author.AuthorId;
+                IsFollowed = false;
+
+                foreach(int t in ids)
+                {   
+                    if(id == t)
+                    {
+                        IsFollowed = true;
+                        break;
+                    }
+                    
+                }
+                if (IsFollowed)
+                {
+                    Cheeps.Add(new CheepViewModel(row.Author.Name, row.Text, row.TimeStamp.ToString(), row.Author.Email, "Unfollow"));
+                    continue;
+                }
+                Cheeps.Add(new CheepViewModel(row.Author.Name, row.Text, row.TimeStamp.ToString(), row.Author.Email, "Follow"));
+
+            }
         }
         returnList = await _service.GetCheepsFromAuthor(author, page);
         foreach (var row in returnList)
@@ -53,6 +77,54 @@ public class UserTimelineModel(ICheepService service) : PageModel
             Cheeps.Add(new CheepViewModel(row.Author.Name, row.Text, row.TimeStamp.ToString(), row.Author.Email, "Follow"));
         }
 
-        return RedirectToPage("Public");
+        return RedirectToPage("UserTimeline");
     }
+
+    [BindProperty]
+    public string Email { get; set; }
+
+    public async Task<IActionResult> OnPostFollow([FromQuery] int page = 0)
+    {
+        var id = await _service.GetAuthorId(Email);
+        var author = await _service.GetEmail(User.Identity.Name, page);
+        var IsFollowed = false;
+
+        var followers = await _service.GetFollowers(author.Email);
+        foreach(int t in followers)
+        {   
+            if(id == t)
+            {
+
+                IsFollowed = true;
+                break;
+            }
+            else
+            {
+                IsFollowed = false;
+            }
+        }
+
+        if (!IsFollowed)
+        {
+            _service.AddFollowerId(author, id);
+        }
+        else
+        {
+            _service.RemoveFollowerId(author, id);
+        }
+
+        followers = await _service.GetFollowers(User.Identity.Name);
+
+        Console.WriteLine(User.Identity.Name + "You are following these people:");
+
+        foreach(int t in followers)
+        {
+            Console.WriteLine(t);
+        }
+
+
+
+        return RedirectToPage("UserTimeline");
+    } 
+
 }
