672143d00ec7d07f40bced6022a219879259454b
Maja-Box <madeleine.jakobsen2@gmail.com>
2025-11-28 16:30:01 +0100

    commiting to push

--- src/Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using Core;
 using Infrastructure;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -130,6 +131,7 @@ namespace Web.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
